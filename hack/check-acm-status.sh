765ad64b2708005e15c9fd24c9f2c9a65abd9c70
Chris Doan <cdoan@redhat.com>
2022-10-27 15:17:04 -0500

    update cluster check script

--- hack/check-acm-status.sh
+++ hack/check-acm-status.sh
@@ -8,31 +8,43 @@ NS2=multicluster-engine
 # reason: InstallSucceeded
 
 function check() {
-    if [[ "$1" == "Available" ]] || [[ "$1" == "Succeeded" ]]; then
+    if [[ "$1" == "Available" ]] || [[ "$1" == "Succeeded" ]] || [[ "$1" == "Running" ]]; then
         icon="✅"
     else
-        icon="❌"
+        icon="❌ (Expected: Available | Succeeded | Running)"
     fi
-    echo " $1 $icon"
+    echo "$1 $icon"
 }
 
 rc=$(oc get mch multiclusterhub -n $NS -o yaml | yq '.status.phase')
-echo "current acm operator phase                   : $(check $rc)"
-echo "current acm subscription currentCSV version  : " $(oc get subscription.operator -n $NS -o yaml | yq '.items[].status.currentCSV')
+echo "current mch operator phase                            : $(check $rc)"
+echo "current acm subscription currentCSV version           : "$(oc get subscription.operator -n $NS -o yaml -l operators.coreos.com/advanced-cluster-management.open-cluster-management= | yq '.items[].status.currentCSV')
 
-rc=$(oc get csv advanced-cluster-management.v2.5.2 -n $NS -o yaml | yq '.status.phase')
-echo "current acm csv phase                        : $(check $rc)"
+# echo "current acm sub conditions"
+# oc get sub -n open-cluster-management acm-operator-subscription -ojsonpath="{.status.conditions}" | jq .
+
+rc=$(oc get csv advanced-cluster-management.v2.7.0 -n $NS -o yaml | yq '.status.phase')
+echo "current acm csv phase                                 : $(check $rc)"
+
+# if [[ "$rc" == "Pending" ]]; then
+#     oc get csv advanced-cluster-management.v2.7.0 -n $NS -o yaml | yq '.status'
+# fi
 
 rc=$(oc get mce multiclusterengine -n $NS -o yaml | yq '.status.phase')
-echo "current mce operator phase                   : $(check $rc)"
-echo "current mce subscription currentCSV version  : " $(oc get subscription.operator -n $NS2 -o yaml | yq '.items[].status.currentCSV')
-rc=$(oc get csv multicluster-engine.v2.0.2 -n $NS2 -o yaml | yq '.status.phase')
-echo "current mce csv phase                        : $(check $rc)" 
+echo "current mce operator phase                            : $(check $rc)"
+echo "current mce subscription currentCSV version           : "$(oc get subscription.operator -n $NS2 -o yaml | yq '.items[].status.currentCSV')
+rc=$(oc get csv multicluster-engine.v2.2.0 -n $NS2 -o yaml | yq '.status.phase')
+echo "current mce csv phase                                 : $(check $rc)" 
 
-echo "count current managed clusters               : " $( oc get managedcluster -A | wc -l )
-echo "count current manifestwork                   : " $( oc get manifestwork -A | wc -l )
+echo "count current managed clusters                        : "$( oc get managedcluster -A | wc -l )
+echo "count current manifestwork                            : "$( oc get manifestwork -A | wc -l )
 
 
 echo "Current InstallPlan State"
 oc get installplan -n $NS
 oc get installplan -n $NS2
+
+oc get dexclient -A
+oc get dexserver -A
+oc get idpconfig -A
+oc get placement -A
