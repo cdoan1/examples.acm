e041bf3596ce9e5b00502e48f8e9323300824923
Chris Doan <cdoan@redhat.com>
2022-10-27 20:07:15 -0500

    fix check

--- hack/mce-sanity-check.sh
+++ hack/mce-sanity-check.sh
@@ -11,7 +11,7 @@ export K=$(params.kubeconfig)
 
 if [[ "$K" != "" ]]; then
     echo "Switching kubeconfig context"
-    echo $K | base64 --decode > $TOP/remote.kubeconfig
+    echo "$K" | base64 --decode > $TOP/remote.kubeconfig
     export KUBECONFIG=$TOP/remote.kubeconfig
     oc cluster-info
 fi
