4dbed0501b296bcbefa1197bdc526599ca39d987
Chris Doan <cdoan@redhat.com>
2022-11-10 21:28:23 -0600

    setup nightly

--- gitops/openshift-pipelines/lab-mce/start.sh
+++ gitops/openshift-pipelines/lab-mce/start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-oc delete -f gitops/openshift-pipelines/lab-mce/pipelinerun/pipelinerun_mce_hypershift.yaml
-oc apply -f gitops/openshift-pipelines/lab-mce/pipelinerun/pipelinerun_mce_hypershift.yaml
+# oc delete -f gitops/openshift-pipelines/lab-mce/pipelinerun/pipelinerun_mce_hypershift.yaml
+# oc apply -f gitops/openshift-pipelines/lab-mce/pipelinerun/pipelinerun_mce_hypershift.yaml
+
+oc delete -f gitops/openshift-pipelines/lab-mce/pipelinerun/pipelinerun_mce_hypershift_26_27_e2e.yaml
+oc apply -f gitops/openshift-pipelines/lab-mce/pipelinerun/pipelinerun_mce_hypershift_26_27_e2e.yaml
